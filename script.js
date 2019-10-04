93595114df91d26bae3b80d65a3bd0290836d07a
barryinvisible <31002774+barryinvisible@users.noreply.github.com>
2019-10-04 22:19:19 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var tabs = document.querySelectorAll(".tab-icon");
+contents = document.querySelectorAll(".tab-content");
+
+function addShowOnClick() {
+    var id = this.dataset.id;
+    for (let i = 0; i < contents.length; i++) {
+        if (contents[i].dataset.id && contents[i].dataset.id === id) {
+            contents[i].classList.add("show");
+        } else {
+            contents[i].classList.remove("show");
+        }
+    }
+}
